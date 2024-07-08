697ff35ad088448c7e3fbf7bbadd1bec4475cede
peterjbone <peterjbone41@gmail.com>
2024-07-08 15:28:21 -0500

    Review column component error solved

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -4,6 +4,7 @@ import { HTMLAttributes, useEffect, useRef, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
 import { cn } from "@/lib/utils";
+import Phone from "./Phone";
 
 const PHONES = [
 	"testimonials/1.jpg",
@@ -28,6 +29,7 @@ function splitArray<T>(array: Array<T>, numParts: number) {
 	return result;
 }
 
+//************************************ REVIEWCOLUMN COMPONENT
 function ReviewColumn({
 	reviews,
 	className,
@@ -62,29 +64,52 @@ function ReviewColumn({
 		<div
 			ref={columnRef}
 			className={cn("animate-marquee space-y-8 py-4", className)}
-      style={{ "--marquee-duration": duration } as React.CSSProperties}>
-      {reviews.concat(reviews).map((imgSrc, reviewIndex) => {
-          <Review/>
-        })}
-      </div>
+			style={{ "--marquee-duration": duration } as React.CSSProperties}>
+			{reviews.concat(reviews).map((imgSrc, reviewIndex) => (
+				<Review
+					key={reviewIndex}
+					className={reviewClassName?.(reviewIndex % reviews.length)}
+					imgSrc={imgSrc}
+				/>
+			))}
+		</div>
 	);
 }
 
+//************************************ REVIEW COMPONENT
 interface ReviewProps extends HTMLAttributes<HTMLDivElement> {
 	imgSrc: string;
 }
 
 function Review({ imgSrc, className, ...props }: ReviewProps) {
+	const POSSIBLE_ANIMATION_DELAYS = [
+		"0s",
+		"0.1s",
+		"0.2s",
+		"0.3s",
+		"0.4s",
+		"0.5s"
+	];
+
+	const animationDelay =
+		POSSIBLE_ANIMATION_DELAYS[
+			Math.floor(Math.random() * POSSIBLE_ANIMATION_DELAYS.length)
+		];
+
 	return (
 		<div
 			className={cn(
 				"animate-fade-in rounded-[2.25rem] bg-white  p-6 opacity-0 shadow-xl shadow-slate-900/5",
 				className
 			)}
-			{...props}></div>
+			style={{ animationDelay }}
+			{...props}>
+			<Phone imgSrc={imgSrc} />
+		</div>
 	);
 }
 
+//************************************ REVIEWGRID COMPONENT
 function ReviewGrid() {
 	/* prettier-ignore */
 	const containerRef = useRef<HTMLDivElement | null>(null)

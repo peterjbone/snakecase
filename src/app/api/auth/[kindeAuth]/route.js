2f142a2cde62cf73a00155f4d385fb416a02f58d
peterjbone <peterjbone41@gmail.com>
2024-07-20 16:34:34 -0500

    Updating cors

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,14 +1,3 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-// pages/api/auth/logout.js
-import cors, { runMiddleware } from "../../../../lib/cors";
-
-async function handler(req, res) {
-	await handleAuth();
-
-	await runMiddleware(req, res, cors);
-
-	res.status(200).json({ message: "success" });
-}
-
-export const GET = handler();
+export const GET = handleAuth();

8460fcd6d5cbdad64828c96038a05b9a72a37785
peterjbone <peterjbone41@gmail.com>
2024-07-20 16:23:52 -0500

    Updating the cors

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -3,12 +3,12 @@ import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 // pages/api/auth/logout.js
 import cors, { runMiddleware } from "../../../../lib/cors";
 
-export default async function handler(req, res) {
-	// Run the middleware
+async function handler(req, res) {
+	await handleAuth();
+
 	await runMiddleware(req, res, cors);
 
-	// Your logout logic here
 	res.status(200).json({ message: "success" });
 }
 
-export const GET = handleAuth();
+export const GET = handler();
